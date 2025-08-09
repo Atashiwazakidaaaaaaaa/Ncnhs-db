352db2f4b6fd918f80b6c809100df686afba0696
Atashiwazakidaaaaaaaa <202312278@gordoncollege.edu.ph>
2025-08-09 23:13:02 +0800

    debugs

--- src/routes/faculty/+page.svelte
+++ src/routes/faculty/+page.svelte
@@ -327,16 +327,7 @@
               {:else}
                 <h2 class="text-xl md:text-2xl lg:text-3xl font-bold md:pl-3 pr-4 md:pr-0">{schoolYearText.toUpperCase()}</h2>
                 {#if isAdminLoggedIn}
-                  <button
-                    on:click={startEditingSchoolYear}
-                    class="text-white hover:text-yellow-200 transition-colors ml-2"
-                    title="Edit school year"
-                    aria-label="Edit school year"
-                  >
-                    <svg xmlns="http://www.w3.org/2000/svg" class="h-4 w-4" fill="none" viewBox="0 0 24 24" stroke="currentColor">
-                      <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M11 5H6a2 2 0 00-2 2v11a2 2 0 002 2h11a2 2 0 002-2v-5m-1.414-9.414a2 2 0 112.828 2.828L11.828 15H9v-2.828l8.586-8.586z" />
-                    </svg>
-                  </button>
+                  
                 {/if}
               {/if}
             </div>
